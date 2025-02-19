8ba8a1f626f6fe750382cfcb23eac81af1db54cd
Samara Soares <samarasoares1313@gmail.com>
2025-02-19 17:46:41 -0300

    update html css js

--- exercícios/ex017/ex017.js
+++ exercícios/ex017/ex017.js
@@ -1,3 +1,4 @@
+/*
 function tabuada(){
     var num = parseInt(document.getElementById("num").value);
     var resposta = document.getElementById('res');
@@ -9,3 +10,28 @@ function tabuada(){
     
     resposta.innerHTML = tabuada;
   }
+*/
+
+
+function tabuada(){
+  var num = document.getElementById('num')
+  var tab = document.getElementById('tab')
+
+  if(num.value.length == 0){
+    alert('Por favor, digite um número!')
+  } else{
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML = ''
+    while(c <= 10){
+      var item = document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      item.value =`tab${c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
+
+
+
+}
