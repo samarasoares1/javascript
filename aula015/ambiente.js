b7c8e6fdfec4c3efb6cd1eaf533620de0c612930
Samara Soares <samarasoares1313@gmail.com>
2025-02-20 13:14:33 -0300

    update js

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,6 +1,17 @@
 let num = [5,7,3,2]
+
+
 num[5] = 8
 num.push(9)
 num.length
 num.sort()
 console.log(`Nosso vetor é o ${num}`)
+
+
+
+let pos = num.indexOf(7)
+if(pos == -1){
+    console.log('O valor não foi encontrado')
+} else{
+    console.log(`O valor 7 está na posição ${pos}`)
+}
