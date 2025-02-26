aa6e9b571411081da4cb5cd5eefaf2e817206df7
Samara Soares <samarasoares1313@gmail.com>
2025-02-26 17:52:35 -0300

    create exercice 17 js

---
+++ aula17/objeto01.js
@@ -0,0 +1,10 @@
+var amigo = {nome: 'João',
+    sexo: 'M',
+    peso: 85.5,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg.`)
